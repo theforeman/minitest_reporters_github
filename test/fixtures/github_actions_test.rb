cb95b8ce9b5b973b8fd33f13ce4b0f7f13e4543b
Evgeni Golov <evgeni@golov.de>
2026-01-16 08:43:12 +0100

    explicitly load Minitest plugins in fixtures/github_actions_test.rb
    Minitest 6+ needs that
    
    https://github.com/minitest/minitest/blob/v6.0.0/History.rdoc

--- test/fixtures/github_actions_test.rb
+++ test/fixtures/github_actions_test.rb
@@ -3,6 +3,7 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest_reporters_github'
 
+Minitest.load_plugins
 Minitest::Reporters.use! MinitestReportersGithub.new
 
 class TestClass < Minitest::Test
